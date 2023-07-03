35a58f963118dc0b7769da9610f6d4f8c00f1c73
andrianarivo <andrianarivo.david@gmail.com>
2023-07-03 16:32:36 +0300

    build header

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,14 +4,44 @@ import { motion } from 'framer-motion';
 export default function Header() {
   return (
     <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
-      <motion.div className="flex flex-row items-center">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center"
+      >
         {/* Social Icons */}
         <SocialIcon url="http://www.linkedin.com/andrianarivo" fgColor="gray" bgColor="transparent" />
         <SocialIcon url="http://www.linkedin.com/andrianarivo" fgColor="gray" bgColor="transparent" />
         <SocialIcon url="http://www.linkedin.com/andrianarivo" fgColor="gray" bgColor="transparent" />
       </motion.div>
 
-      <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
         <SocialIcon
           className="cursor-pointer"
           network="email"
@@ -19,7 +49,7 @@ export default function Header() {
           bgColor="transparent"
         />
         <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get in Touch</p>
-      </div>
+      </motion.div>
     </header>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,6 @@ export default function Home() {
         <title>David&apos;s portfolio</title>
       </Head>
 
-      {/* Header */}
       <Header />
 
       {/* Hero */}
