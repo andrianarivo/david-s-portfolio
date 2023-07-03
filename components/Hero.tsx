7636b55c9ec73fb23d6cc653beeff4f73c9b35f3
andrianarivo <andrianarivo.david@gmail.com>
2023-07-03 18:17:11 +0300

    build hero

---
+++ components/Hero.tsx
@@ -0,0 +1,47 @@
+import { Cursor, useTypewriter } from 'react-simple-typewriter';
+import BackgroundCircles from '@/components/BackgroundCircles';
+import Link from 'next/link';
+
+export default function Hero() {
+  const [text, count] = useTypewriter({
+    words: [
+      'Designer',
+      'Coder',
+      'Leader',
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <img
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src="https://avatars.githubusercontent.com/u/32914511?v=4"
+        alt="profile"
+      />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Fullstack Engineer</h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#f7ab0a" />
+        </h1>
+
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton" type="button">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton" type="button">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton" type="button">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton" type="button">Projects</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import { Inter } from 'next/font/google';
 import Head from 'next/head';
 import Header from '@/components/Header';
+import Hero from '@/components/Hero';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <main
-      className={`${inter.className}`}
+      className={`${inter.className} bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0`}
     >
       <Head>
         <title>David&apos;s portfolio</title>
@@ -16,6 +17,9 @@ export default function Home() {
       <Header />
 
       {/* Hero */}
+      <section id="hero" className="snap-center">
+        <Hero />
+      </section>
 
       {/* About */}
 
