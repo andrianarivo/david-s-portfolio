e33348d98268cd8b1bcc057a4aca7de4a3d425f3
Andrianarivo David <andrianarivo.david@gmail.com>
2023-07-05 19:20:02 +0300

    fix hydration error

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,16 @@
 import { SocialIcon } from 'react-social-icons';
 import { motion } from 'framer-motion';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { SyntheticEvent } from 'react';
 
 export default function Header() {
+  const router = useRouter();
+
+  const handleClick = (e: SyntheticEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    router.push('#contact');
+  };
+
   return (
     <header
       className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center"
@@ -40,32 +48,31 @@ export default function Header() {
         />
       </motion.div>
 
-      <Link href="#contact">
-        <motion.div
-          initial={{
-            x: 500,
-            opacity: 0,
-            scale: 0.5,
-          }}
-          animate={{
-            x: 0,
-            opacity: 1,
-            scale: 1,
-          }}
-          transition={{
-            duration: 1.5,
-          }}
-          className="flex flex-row items-center text-gray-300 cursor-pointer"
-        >
-          <SocialIcon
-            className="cursor-pointer"
-            network="email"
-            fgColor="gray"
-            bgColor="transparent"
-          />
-          <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get in Touch</p>
-        </motion.div>
-      </Link>
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        onClick={handleClick}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get in Touch</p>
+      </motion.div>
     </header>
   );
 }
