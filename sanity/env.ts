1aab0c48efa21d5c7ffeb961aa35984ec602da1e
andrianarivo <andrianarivo.david@gmail.com>
2023-07-06 17:15:59 +0300

    update sanity env

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,12 +1,12 @@
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-07-05';
 
 export const dataset = assertValue(
-    process.env.SANITY_STUDIO_DATASET,
-    'Missing environment variable: DATASET'
+    process.env.NEXT_PUBLIC_SANITY_DATASET,
+    `Missing environment variable: DATASET ${process.env.NEXT_PUBLIC_SANITY_DATASET}`
 );
 
 export const projectId = assertValue(
-    process.env.SANITY_STUDIO_PROJECT_ID,
+    process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     'Missing environment variable: PROJECT_ID',
 );
 
